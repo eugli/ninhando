7a62b0422477597b1fbeb64f2a83487fbd185d8a
benny <bennycortese@gmail.com>
2022-10-22 19:23:22 -0400

    testing emulator

--- src/App/Game/index.js
+++ src/App/Game/index.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import './index.scss';
 import Detection from './Detection';
+import Emulator from './Emulator';
 
 const keyEvent = (key) => {
     const event = new KeyboardEvent('keydown', { key });
@@ -15,13 +16,35 @@ const TETRIS_GESTURE_KEY_MAP = {
 }
 
 const Game = () => {
-
     const [gesture, setGesture] = useState('waiting for gesture');
+    const [romData, setRomData] = useState('');
+    const [paused, setPaused] = useState(false);
+
+    useEffect(() => {
+        let reader = new FileReader();
+        console.log("hi");
+        
+        url = "http://localhost:3000/roms/tetris/tetris.nes";
+        loadBinary(
+            url,
+            (err, data) => {
+                if (err) {
+                   console.log("error");
+                } else {
+                    setRomData(data);
+                }
+            }
+        );
+    })
 
     return (
         <div className="Game">
             <div className="game-container">
-                <h1>Game</h1>
+                <h1>HI HELO</h1>
+                {/* <Emulator>
+                    romData={""}
+                     paused={false}
+                </Emulator> */}
             </div>
             <div className='controller-container'>
                 <div className="controller">
