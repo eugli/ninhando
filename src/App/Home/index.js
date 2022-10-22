3a7b5fb26924c8822326a761b46438b878f9fca5
eugli <lieugenebaba01@gmail.com>
2022-10-22 14:39:48 -0400

    added carousel

--- src/App/Home/index.js
+++ src/App/Home/index.js
@@ -1,28 +1,37 @@
 
 import React from 'react';
+import { Box } from '@mui/material';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from 'react-responsive-carousel';
 import './index.scss';
 
 const Home = () => {
     return (
-        <div className="Home">
-            <h1>Hello World!</h1>
-            <h1>Hello World!</h1>
-            <h1>Hello World!</h1>       
-            <h1>Hello World!</h1>
-            <h1>Hello World!</h1>
-            <h1>Hello World!</h1>     
-            <h1>Hello World!</h1>
-            <h1>Hello World!</h1>
-            <h1>Hello World!</h1>     
-            <h1>Hello World!</h1>
-            <h1>Hello World!</h1>
-            <h1>Hello World!</h1>     
-            <h1>Hello World!</h1>
-            <h1>Hello World!</h1>
-            <h1>Hello World!</h1>     
-            <h1>Hello World!</h1>
-            <h1>Hello World!</h1>
-            <h1>Hello World!</h1>        
+        <div>
+            <Box className="Carousel" sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}>
+                <Carousel>
+                    <div>
+                        <img src="/images/pacman.png"
+                             alt="pacman"
+                        />
+                        <p className="legend">Pacman</p>
+                    </div>
+                    <div>
+                        <img src="/images/space-invaders.png"
+                             alt="space-invaders"/>
+                        <p className="legend">Space Invaders</p>
+                    </div>
+                    <div>
+                        <img src="/images/tetris.png"
+                             alt="tetris"/>
+                        <p className="legend">Tetris</p>
+                    </div>
+                </Carousel>
+            </Box>
         </div>
     );
 }
